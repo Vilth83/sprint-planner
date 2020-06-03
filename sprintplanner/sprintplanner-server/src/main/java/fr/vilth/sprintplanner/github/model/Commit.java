02ac05a0eaab10ffc154190ea0ce91d1b2122329
Thierry VILLEPREUX <44159613+Vilth83@users.noreply.github.com>
2020-06-03 17:23:35 +0200

    [SPL-28] compare current branch with previous one

--- sprintplanner/sprintplanner-server/src/main/java/fr/vilth/sprintplanner/github/model/Commit.java
+++ sprintplanner/sprintplanner-server/src/main/java/fr/vilth/sprintplanner/github/model/Commit.java
@@ -1,8 +1,64 @@
 package fr.vilth.sprintplanner.github.model;
 
+import java.util.Objects;
+
 public class Commit {
 
-    private String sha;
+	private String sha;
 
-    private String url;
+	private CommitDetails commit;
+
+	private String html_url;
+
+	private String key;
+
+	public String getMessage() {
+		return commit.message;
+	}
+
+	public String getCommitDate() {
+		return commit.author.date;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getHtmlUrl() {
+		return html_url;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(key);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Commit)) {
+			return false;
+		}
+		Commit other = (Commit) obj;
+		return Objects.equals(key, other.key);
+	}
+}
+
+class CommitDetails {
+
+	String sha;
+	String url;
+	Author author;
+	String message;
+}
+
+class Author {
+	String name;
+	String date;
 }
